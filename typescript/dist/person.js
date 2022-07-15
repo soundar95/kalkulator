cb3ec0fa705a0dd0df8c6f3628102a1e368751ee
soundar95 <soundarrajanv95@gmail.com>
2022-07-15 17:20:53 +0530

    typescript

---
+++ typescript/dist/person.js
@@ -0,0 +1,5 @@
+"use strict";
+const person = "rajan";
+const age = 26;
+console.log(`my name is ${person} and my age is ${age}`);
+console.log("my name is" + " " + person + "and my age is" + " " + age);

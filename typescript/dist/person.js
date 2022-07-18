b53b2013a16ed4d934e22e5be8383b1428d29da4
soundar95 <soundarrajanv95@gmail.com>
2022-07-18 15:37:57 +0530

    nestapplication

--- typescript/dist/person.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const person = "rajan";
-const age = 26;
-console.log(`my name is ${person} and my age is ${age}`);
-console.log("my name is" + " " + person + "and my age is" + " " + age);
--- typescript/src/person.ts
+++
@@ -1,4 +0,0 @@
-const person:string = "rajan";
-const age:number = 26;
-console.log(`my name is ${person} and my age is ${age}`);
-console.log("my name is" + " " + person + "and my age is" + " " + age);

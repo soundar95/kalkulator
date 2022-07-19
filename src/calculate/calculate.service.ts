17d709329a3a59669238f3a2aacec9fea929cf69
Basanta <basanta.goswami@zysk.in>
2022-07-19 11:54:09 +0530

    task 7: create location module and apis

--- src/calculate/calculate.service.ts
+++ src/calculate/calculate.service.ts
@@ -3,36 +3,31 @@ import { ValuesDto } from './dto/values.dto';
 
 @Injectable()
 export class CalculateService {
-  private values=[];
-  createValues(valuesDto:ValuesDto){
-   const {value}=valuesDto;
-   this.values = value;
-   return value;
+  private values = [];
+  createValues(valuesDto: ValuesDto) {
+    const { value } = valuesDto;
+    this.values = value.map((num) => Number(num));
+    return value;
   }
 
   getCalculateBySum(): number | string {
     let sum = 0;
-    let abc=this.values.map(a=>Number(a));
-    for (let i = 0; i < abc.length; i++) {
-      sum += abc[i];
+    for (let i = 0; i < this.values.length; i++) {
+      sum += this.values[i];
     }
     return `the sum of the numbers is ${sum}`;
   }
   getCalculateByMin(): number | string {
-    let abc=this.values.map(a=>Number(a));
-    let min: number = abc[0];
-    for (let i = 1; i < abc.length; i++) {
-      if (min > abc[i]) 
-      min = abc[i];
+    let min: number = this.values[0];
+    for (let i = 1; i < this.values.length; i++) {
+      if (min > this.values[i]) min = this.values[i];
     }
     return `the minimum value is ${min}`;
   }
   getCalculateByMax(): number | string {
-    let abc=this.values.map(a=>Number(a));
-    let max: number = abc[0];
-    for (let i = 1; i < abc.length; i++) {
-      if (max < abc[i]) 
-      max = abc[i];
+    let max: number = this.values[0];
+    for (let i = 1; i < this.values.length; i++) {
+      if (max < this.values[i]) max = this.values[i];
     }
     return `the maximum value is ${max}`;
   }
